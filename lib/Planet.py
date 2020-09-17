814d5e31d8f98bc758e5120b441957bd91255b68
Iridium77Ir <65957631+Iridium77Ir@users.noreply.github.com>
2020-09-17 17:36:27 +0200

    one linerised

--- lib/Planet.py
+++ lib/Planet.py
@@ -4,27 +4,13 @@ import math
 
 class Planet():
     def __init__(self, mass, accelerationx, accelerationy, positionx, positiony):
-        self.mass = mass
-        self.accelerationx = accelerationx
-        self.accelerationy = accelerationy
-        self.positionx = positionx
-        self.positiony = positiony
+        self.mass, self.accelerationx, self.accelerationy, self.positionx, self.positiony = mass, accelerationx, accelerationy, positionx, positiony
 
     def distance(self, self2):
-        distance = math.sqrt((math.pow(abs(self2.positionx - self.positionx), 2))+(math.pow(abs(self2.positiony - self.positiony),2)))
-        return distance
+        return math.sqrt((math.pow(abs(self2.positionx - self.positionx), 2))+(math.pow(abs(self2.positiony - self.positiony),2)))
 
     def move(self, self2, sigma):
         distance = self.distance(self2)
-        cos = ((abs(self.positionx - self2.positionx))/distance)
-        sin = ((abs(self.positiony - self2.positiony))/distance)
         force = ((0.0000000000667 * ((self2.mass)/(math.pow(distance, 2)))))
-
-        if self.positionx < self2.positionx:
-            self.accelerationx += ((abs(cos*force))/2*sigma)
-        elif self.positionx > self2.positionx:
-            self.accelerationx -= ((abs(cos*force))/2*sigma)
-        if self.positiony < self2.positiony:
-            self.accelerationy += ((abs(sin*force))/2*sigma)
-        elif self.positiony > self2.positiony:
-            self.accelerationy -= ((abs(sin*force))/2*sigma)
+        self.accelerationx -= ((((self.positionx - self2.positionx)/distance)*force)/2*sigma)
+        self.accelerationy -= ((((self.positiony - self2.positiony)/distance)*force)/2*sigma)
--- lib/Save.py
+++ lib/Save.py
@@ -2,14 +2,12 @@ import numpy as np
 
 class Save(): 
     def __init__(self):
-        self.posxarr = np.delete(np.array([0], dtype='int64'), 0)
-        self.posyarr = np.delete(np.array([0], dtype='int64'), 0)
+        self.posxarr, self.posyarr = np.delete(np.array([0], dtype='int64'), 0), np.delete(np.array([0], dtype='int64'), 0)
 
     def save(self, pointx, pointy):
         self.posxarr = np.append(self.posxarr, pointx)
         self.posyarr = np.append(self.posyarr, pointy)
 
     def export(self, totaltime, numofplan, filename):
-        self.posxarr = np.reshape(self.posxarr, (totaltime, numofplan))
-        self.posyarr = np.reshape(self.posyarr, (totaltime, numofplan))
+        self.posxarr, self.posyarr = np.reshape(self.posxarr, (totaltime, numofplan)), np.reshape(self.posyarr, (totaltime, numofplan))
         np.savez_compressed(filename, posxarr=self.posxarr, posyarr=self.posyarr)
